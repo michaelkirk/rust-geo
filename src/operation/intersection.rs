c9f0bb4b159c37c24dd73775da6d617661e0e84e
Michael Kirk <michael.john.kirk@gmail.com>
2016-06-03 19:04:42 -0700

    intersect point on line

--- src/operation/intersection.rs
+++ src/operation/intersection.rs
@@ -1,4 +1,4 @@
-use types::Point;
+use types::{Point, LineString};
 
 pub trait HasIntersection<T> {
     fn intersection(&self, rhs: &T) -> Option<Point>;
@@ -27,5 +27,73 @@ impl HasIntersection<Point> for Point {
         } else {
             return None;
         }
+    }
+}
+
+impl HasIntersection<Point> for LineString {
+    /// Returns the point if it's on the LineString.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use geo::{Point, LineString};
+    /// use geo::operation::intersection::HasIntersection;
+    ///
+    /// let line_start = Point::new(1.0, 1.0);
+    /// let in_line = Point::new(2.0, 2.0);
+    /// let line_string = LineString(vec![
+    ///   Point::new(1.0, 1.0),
+    ///   Point::new(3.0, 3.0)
+    /// ]);
+    ///
+    /// assert_eq!(Some(line_start), line_string.intersection(&line_start));
+    /// assert_eq!(Some(in_line), line_string.intersection(&in_line));
+    ///
+    /// let off_line = Point::new(1.0, 2.0);
+    /// assert_eq!(None, line_string.intersection(&off_line));
+    ///
+    /// let past_line = Point::new(4.0, 4.0);
+    /// assert_eq!(None, line_string.intersection(&past_line));
+    ///
+    /// let past_line = Point::new(1.0, 4.0);
+    /// let vertical_line_string = LineString(vec![
+    ///   Point::new(1.0, 1.0),
+    ///   Point::new(1.0, 3.0)
+    /// ]);
+    /// assert_eq!(None, vertical_line_string.intersection(&past_line));
+    /// ```
+    fn intersection(&self, point: &Point) -> Option<Point> {
+        for (start, end) in self.0.iter().zip(self.0[1..].iter()) {
+            let dx_point = point.x() - start.x();
+            let dy_point = point.y() - start.y();
+            let dx_line = end.x() - start.x();
+            let dy_line = end.y() - start.y();
+
+            let cross_product_magnitude = dx_point * dy_line - dy_point * dx_line;
+
+            if cross_product_magnitude != 0. {
+                continue;
+            }
+
+            // point is on the line extending from the segment, but is it within the segment?
+
+            let coord = if dx_line == 0. {
+                // All points on a vertical line have the same x, so we must compare y values
+                Point::y
+            } else {
+                Point::x
+            };
+
+            let (lower_bound, upper_bound) = if coord(start) < coord(end) {
+                (coord(start), coord(end))
+            } else {
+                (coord(end), coord(start))
+            };
+
+            if lower_bound <= coord(point) && coord(point) <= upper_bound {
+                return Some(point.clone());
+            }
+        }
+        return None;
     }
 }
